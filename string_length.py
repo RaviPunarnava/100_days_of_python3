90178738796c99b8a330edc75aba3a6035543927
H R <shr@EBR-SHR2-MAC.local>
2019-10-09 10:09:24 +0530

    a simple string length implementation

---
+++ string_length.py
@@ -0,0 +1,11 @@
+# a simple Python program to demonstrate the use of len() method   
+  
+#example 1
+string = "abracadabra" 
+print ("the String is 'abracadabra' and its length is ")
+print(len(string)) 
+  
+#example 2
+string = "PKMKB" 
+print ("The String is 'PKMKB' and its legth is")
+print(len(string))
