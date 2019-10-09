3bf8f15e90d0cb62ce7a0723fb2934fc37b33091
H R <shr@EBR-SHR2-MAC.local>
2019-10-09 09:53:36 +0530

    modified to print all possible values

--- date_parser.py
+++ date_parser.py
@@ -14,4 +14,17 @@ mi = str(now.minute)
 
 ss = str(now.second)
 
-print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+print ("Current time with timestamp")
+print (now)
+
+print ("current time without timestamp")
+print (mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
+
+
+print "Current year: %d" % now.year
+print "Current month: %d" % now.month
+print "Current day: %d" % now.day
+print "Current hour: %d" % now.hour
+print "Current minute: %d" % now.minute
+print "Current second: %d" % now.second
+print "Current microsecond: %d" % now.microsecond
